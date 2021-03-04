bee063ab71c62f3c16089391c3abafd920e8fbfd
JorgeBorges <jorgetrabalho2016@gmail.com>
2021-03-04 08:58:16 -0300

    Implementation a main programming with knowledge got in the live session.

--- lib/study_diary.rb
+++ lib/study_diary.rb
@@ -1,49 +1,67 @@
+require_relative 'study_item'
+
+REGISTER = 1
+VIEW     = 2
+SEARCH   = 3
+EXIT     = 4
+
+def clear
+  system('clear')
+end
+
+def wait_keypress
+  puts
+  puts 'Pressione enter para continuar.'
+  gets
+end
+
+def clear_and_wait_keypress
+  wait_keypress
+  clear
+end
+
 def welcome
   'Bem-vindo ao Diário de Estudos, seu companheiro para estudar!'
 end
 
 def menu
-  puts "[1] Cadastrar um item para estudar"
-  puts "[2] Ver todos os itens cadastrados"
-  puts "[3] Buscar um item de estudo"
-  puts "[4] Sair"
+  puts "[#{REGISTER}] Cadastrar um item para estudar"
+  puts "[#{VIEW}] Ver todos os itens cadastrados"
+  puts "[#{SEARCH}] Buscar um item de estudo"
+  puts "[#{EXIT}] Sair"
   print 'Escolha uma opção: '
   gets.to_i
 end
 
-def register_study_item
-  print 'Digite o título do seu item de estudo: '
-  title = gets.chomp
-  print 'Digite a categoria do seu item de estudo: '
-  category = gets.chomp
-  puts "Item '#{title}' da categoria '#{category}' cadastrado com sucesso!"
-  { title: title, category: category }
+def search_items
+  print 'Digite uma palavra para procurar: '
+  term = gets.chomp
+  found_items = StudyItem.all.filter do |item|
+    item.include?(term,"title")
+  end
 end
 
+clear
 puts welcome
-study_items = []
 option = menu
 
-while option != 4
-  if option == 1
-    study_items << register_study_item
-  elsif option == 2
-    study_items.each_with_index do |item, index|
-      puts "##{index + 1} - #{item[:title]} - #{item[:category]}"
-    end
-    puts 'Nenhum item cadastrado' if study_items.empty?
-  elsif option == 3
-    print 'Digite uma palavra para procurar: '
-    term = gets.chomp
-    found_items = study_items.filter do |item|
-      item[:title].include? term
-    end
-    puts found_items
-    puts 'Nenhum item encontrado' if found_items.empty?
+loop do
+  clear
+  case option
+  when REGISTER
+    StudyItem.register
+  when VIEW
+    puts StudyItem.all
+    puts 'Nenhum item encontrado' if StudyItem.all.empty?
+  when SEARCH
+    puts search_items
+  when EXIT
+    clear
+    puts 'Obrigado por usar o Diário de Estudos'
+    break
   else
-    puts 'Opção inválida'
+    puts "Opção inválida"
   end
+  clear_and_wait_keypress
   option = menu
 end
-
-puts 'Obrigado por usar o Diário de Estudos'  
