bc9fdb2d1f8f3b9e2ff106eb9685023738c14807
JorgeBorges <jorgetrabalho2016@gmail.com>
2021-03-04 08:55:11 -0300

    Refactoring search, implementation a generic search.

--- lib/study_item.rb
+++ lib/study_item.rb
@@ -1,48 +1,46 @@
+require 'pry'
+
 class StudyItem
 	attr_reader :id, :title, :category
 
 	@@next_id = 1
 	@@study_items = []
 
-	def initialize(title, category)
-		@id = @next_id
+	def initialize(title:, category:)
+		@id = @@next_id
 		@title = title
 		@category = category
 
-		@next_id += 1
+		@@next_id += 1
 		@@study_items << self
 	end
 
-	def include?(query,*attributes)
-		attributes.each { |attribute| attribute.include?(query) }
+	def include?(query,attribute)
+		send(attribute).include?(query)
 	end
 
 	def to_s
 		"##{id} - #{title} - #{category}"
 	end
 
-	def self.register 
-		print "Dicite o titulo do seu item de estudo: "
+	def self.register
+		print "Digite o titulo do seu item de estudo: "
 		title = gets.chomp
 		print "Digite a categoria do seu item de estudo: "
-		category = get.chomp
+		category = gets.chomp
 		puts "Item '#{title}' da categoria '#{category}' cadastrado com sucesso!"
-		new(title: title, category: category)
+		self.new(title: title, category: category)
 	end
 
 	def self.all
 		@@study_items
 	end
 
-	def self.search(term)
-	end
+	def self.search(term);end
 
-	def self.print
-	end
-
-	def self.delete 
+	def self.delete
 		puts '==== List de items ===='
-		
+
 		puts 'Qual o id do Item de estudo você quer apagar?'
 		id = gets.to_i
 		study_item = StudyItem.all.detect do |study_item|
