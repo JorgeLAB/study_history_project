76086fa6647f9a9818cf28e8fe62277a7fe3f879
JorgeBorges <jorgetrabalho2016@gmail.com>
2021-03-13 00:33:24 -0300

    Add validation for study_item model.

--- app/models/study_item.rb
+++ app/models/study_item.rb
@@ -1,6 +1,9 @@
 class StudyItem < ApplicationRecord
 
+  validates :title, presence: true
+  validates :description, presence: true
+
   def html_title
-    "#{category.name} - #{title}"
+    "#{category} - #{title}"
   end
 end
